290df301e00a2b2d8ffda30113ca3428ee7a02dc
fabianallvarez <fabianallvarez@gmail.com>
2022-07-05 19:59:12 +0200

    FT: La tabla Funciona

--- balance/__init__.py
+++ balance/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
 
+
+FICHERO = "balance/data/movimientos.csv"
+
 app = Flask(__name__)
 
--- balance/views.py
+++ balance/views.py
@@ -1,10 +1,15 @@
-from balance import app
+from flask import render_template
+
+from . import app
+from .models import ListaMovimientos
 
 
 @app.route('/')
 def home():
-    return "Pagina de inicio"
-
+    movimientos = ListaMovimientos()
+    movimientos.leer_archivo()
+    return render_template("inicio.html", movs=movimientos.lista_movimientos)
+  
 @app.route('/nuevo')
 def nuevo():
     return "Creacion de movimiento"
