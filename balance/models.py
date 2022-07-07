65e8f1449f254cc1d33c2f0e0444ea34f233ede7
fabianallvarez <fabianallvarez@gmail.com>
2022-07-07 20:25:13 +0200

    FT: clase del martes

--- balance/models.py
+++ balance/models.py
@@ -3,27 +3,38 @@ Un movimiento debe tener:
 
 1. Fecha
 2. Concepto
-3, Tipo/I-ngreso, G-asto)
+3. Tipo (I-ngreso, G-asto)
 4. Cantidad
 """
 import csv
+from datetime import date, datetime
 
 from . import FICHERO
 
-class movimiento:
+
+class Movimiento:
     def __init__(self, fecha, concepto, tipo, cantidad):
-        self.fecha = fecha
+        self.errores = []
+        try:
+            self.fecha = date.fromisoformat(fecha)
+        except ValueError:
+            self.fecha = None
+            self.errores.append("El formato de la fecha no es válido")
+
         self.concepto = concepto
         self.tipo = tipo
         self.cantidad = cantidad
 
+
 class ListaMovimientos:
     def __init__(self):
-        self.lista_movimientos = []
+        self.movimientos = []
 
     def leer_archivo(self):
-        with open(FICHERO, "r", encoding="UTF-8")as fichero:
+        with open(FICHERO, "r") as fichero:
             reader = csv.DictReader(fichero)
             for linea in reader:
-                self.lista_movimientos.append(linea)
-
+                mov = Movimiento(
+                    linea["fecha"], linea["concepto"],
+                    linea["tipo"], linea["cantidad"])
+                self.movimientos.append(mov)
--- balance/views.py
+++ balance/views.py
@@ -6,17 +6,23 @@ from .models import ListaMovimientos
 
 @app.route('/')
 def home():
+    """
+    Muestra la lista de movimientos cargados.
+    """
     movimientos = ListaMovimientos()
     movimientos.leer_archivo()
-    return render_template("inicio.html", movs=movimientos.lista_movimientos)
-  
+    return render_template("inicio.html", movs=movimientos.movimientos)
+
+
 @app.route('/nuevo')
 def nuevo():
-    return "Creacion de movimiento"
+    return render_template("nuevo.html")
+
 
 @app.route('/modificar')
 def actualizar():
     return "Actualizar movimiento"
+
 
 @app.route('/borrar')
 def borrar():
